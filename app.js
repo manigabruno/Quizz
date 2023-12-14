79814df2c6adaeb6e7665fe8f5fc870c7ce3fefc
kadie972 <codeurfou@outlook.com>
2023-12-14 23:49:22 +0100

    ajout numero de question et eslint

---
+++ app.js
@@ -0,0 +1,25 @@
+const responses = ["c", "a", "b", "a", "c"];
+const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+const form = document.querySelector(".quiz-form");
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e){
+    e.preventDefault()
+
+    const results = [];
+    
+    const radioButtons = document.querySelectorAll("input[type='radio']:checked")
+    
+    radioButtons.forEach((radioButton, index) => {
+        if (radioButton.value === responses[index]){
+            results.push(true)
+    
+        }
+    })
+
+    
+    
+    console.log(radioButtons);
+}    
+
